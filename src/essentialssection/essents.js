1e7ac42a7b544db1a6870f5497dda246015dcb64
lmqnhakim <lqmnhakim32@gmail.com>
2023-07-28 02:04:15 +0800

    trial pt.2

--- src/essentialssection/essents.js
+++ src/essentialssection/essents.js
@@ -1,44 +1,44 @@
-// import './essents.css'
-// import bottomleft from './assets/bottomleft.png'
-// import bottomright from './assets/bottomright.png'
-// import midtop from './assets/midtop.png'
-// import topleft from './assets/topleft.png'
-// import topright from './assets/topright.png'
-// import midbox from './assets/midbox.png'
+import './essents.css'
+import bottomleft from './assets/bottomleft.png'
+import bottomright from './assets/bottomright.png'
+import midtop from './assets/midtop.png'
+import topleft from './assets/topleft.png'
+import topright from './assets/topright.png'
+import midbox from './assets/midbox.png'
 
-// function EssentialSect() {
-//     return (
-//         <div className='mainbox'>
-//         <div className='firstrow'>
-//             <div className='leftbox topbox'>
-//                 <img className='top sze' src={topleft} alt='top photo'></img>
-//                 <button className='topbtn'>TOP</button>
-//             </div>
-//             <div className='midbox topbox'>
-//                 <img className='top sze' src={midtop} alt='midtop photo'></img>
-//                 <button className='topbtn'>OUTERWEAR</button>
-//             </div>
-//             <div className='rightbox topbox'>
-//                 <img className='top sze' src={topright} alt='top photo'></img>
-//                 <button className='topbtn'>SNKRS</button>
-//             </div>
-//         </div>
-//         <div className='secondrow'>
-//             <div className='leftbox botbox'>
-//                 <img className='bot sze' src={bottomleft} alt='pants'></img>
-//                 <button className='botbtn'>BOTTOM</button>
-//             </div>
-//             <div className='midbox botbox'>
-//                 <img className='bot sze' src={midbox} alt='accessories'></img>
-//                 <button className='botbtn'>ACCESSORIES</button>
-//             </div>
-//             <div className='rightbox botbox'>
-//                 <img className='bot sze' src={bottomright} alt='sneakers'></img>
-//                 <button className='botbtn'>SNKRS</button>
-//             </div>
-//         </div>
-//         </div>
-//     )
-// }
+function EssentialSect() {
+    return (
+        <div className='mainbox'>
+        <div className='firstrow'>
+            <div className='leftbox topbox'>
+                <img className='top sze' src={topleft} alt='top photo'></img>
+                <button className='topbtn'>TOP</button>
+            </div>
+            <div className='midbox topbox'>
+                <img className='top sze' src={midtop} alt='midtop photo'></img>
+                <button className='topbtn'>OUTERWEAR</button>
+            </div>
+            <div className='rightbox topbox'>
+                <img className='top sze' src={topright} alt='top photo'></img>
+                <button className='topbtn'>SNKRS</button>
+            </div>
+        </div>
+        <div className='secondrow'>
+            <div className='leftbox botbox'>
+                <img className='bot sze' src={bottomleft} alt='pants'></img>
+                <button className='botbtn'>BOTTOM</button>
+            </div>
+            <div className='midbox botbox'>
+                <img className='bot sze' src={midbox} alt='accessories'></img>
+                <button className='botbtn'>ACCESSORIES</button>
+            </div>
+            <div className='rightbox botbox'>
+                <img className='bot sze' src={bottomright} alt='sneakers'></img>
+                <button className='botbtn'>SNKRS</button>
+            </div>
+        </div>
+        </div>
+    )
+}
 
-// export default EssentialSect;
+export default EssentialSect;
