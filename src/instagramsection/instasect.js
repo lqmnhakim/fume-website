ce719b845b4e60f1e9ab762f5efbd1de9cd0a7b1
lmqnhakim <lqmnhakim32@gmail.com>
2023-07-28 22:07:54 +0800

    insta section trial A

--- src/instagramsection/instasect.js
+++ src/instagramsection/instasect.js
@@ -1,13 +1,13 @@
-
-import './instasect.css'
 import React, { useState, useEffect } from 'react';
+import './instasect.css'
 
 const InstagramPhotos = () => {
-  const userAccessToken = 'IGQVJYOGdpbUp4RlZA5RE03T3JDM2FTZA0pkc0pCNWVpZA2F0dWpYVGVVLW5IcG5kdkZAaV1R2M3VGQ19EZA0hreGI3d0YzQktFQjktMGtPbHU1bHJTRTd6NWc1MlJpbE5DdGhPYXZAJUFNEYjRyWnZAIMzZA3cQZDZD';
-  const numPhotos = 3;
-  const apiUrl = `https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,likes&access_token=${userAccessToken}&limit=${numPhotos}`;
+  const userAccessToken = 'IGQVJWWGlkZAC1CYUxOMHlsY2xfMzRpWVdHR3ZA4VS1kWXFfR095dWgyeUJscWVYelJ2UlB6Yk1zQjlfSkV4V0prSEVtSXpWelllazJ6TVBmSm9QVFdycGU5MV83YkhCM0hPMWphQnkwY3ZACZAHB6MW9iRwZDZD';
+  const numPhotos = 4;
+  const apiUrl = `https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,likes,username&access_token=${userAccessToken}&limit=${numPhotos}`;
 
   const [photosData, setPhotosData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchInstagramData = async () => {
@@ -15,23 +15,34 @@ const InstagramPhotos = () => {
         const response = await fetch(apiUrl);
         const data = await response.json();
         setPhotosData(data.data);
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching Instagram photos:', error);
+        setLoading(false);
       }
     };
 
     fetchInstagramData();
   }, []);
 
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!photosData || photosData.length === 0) {
+    return <p>No photos to display.</p>;
+  }
+
   return (
-    <div className='instagrambox'>
+    <div className='instaboxpicture'>
       {photosData.map((photo) => (
-        <div className='minibox' key={photo.id}>
+        <div className='hvrinsta' key={photo.id}>
           <a href={photo.permalink} target="_blank">
-            <img className='instaimage' src={photo.media_url} alt={photo.caption} />
-          </a> 
-          <p> ❤︎ {photo.likes ? photo.likes.summary.total_count : ' Hidden'}</p>
-          <p>{photo.caption}</p>
+            <img className='hvrbox' src={photo.media_url} alt={photo.caption} />
+          </a>
+          <div>
+          <p><b>@{photo.username}</b> - {photo.caption ? photo.caption : 'No caption'} </p>
+          </div>
         </div>
       ))}
     </div>
@@ -39,4 +50,8 @@ const InstagramPhotos = () => {
 };
 
 export default InstagramPhotos;
+
+
+
+
 
